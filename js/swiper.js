480262eadb7fcaccfbbc7313d1cbd9e9437550d6
HallyLiu <s1094476@g.ncyu.edu.tw>
2024-06-05 13:29:27 +0800

    fix rwd & add swiper

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        }
+      }
+  });
